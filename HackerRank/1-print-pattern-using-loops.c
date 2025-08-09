acdf7126b54aa8a678034d11da0327ac59a80d22
Asad <asadahsan2245@gmail.com>
2025-08-09 19:27:51 +0530

    creating portfolio website

--- HackerRank/1-print-pattern-using-loops.c
+++ HackerRank/1-print-pattern-using-loops.c
@@ -5,10 +5,22 @@
 
 int main() 
 {
-    int n;
-    scanf("%d", &n);
-    // int temp=n;
+    int n=4;
+    // scanf("%d", &n);
+    int temp=n;
+    int p, q;
+    for(int i=0; i<2*n-1; i++){
+      for(int j=0; j<2*n-1; j++){
+        if((i==0) || (i==2*n-1) || (j==0) || (j==2*n-2)){
+          printf("%d ", temp);
+        }
+        else{
+          temp = n - 1;
+          printf("%d ", temp);
+        }
+      }
+      printf("\n");
+    }
   	// Complete the code to print the pattern.
-    printf("\n");
     return 0;
 }
