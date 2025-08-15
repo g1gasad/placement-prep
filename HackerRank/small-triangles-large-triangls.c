468f275e78b5555d8c57e7170b36c58c9b468178
Asad <asadahsan2245@gmail.com>
2025-08-15 21:49:00 +0530

    new questions

--- HackerRank/small-triangles-large-triangls.c
+++ HackerRank/small-triangles-large-triangls.c
@@ -15,29 +15,24 @@ void sort_by_area(triangle* tr, int n) {
 	* Sort an array a of the length n
 	*/
     int arr[n];
-    int x, y, z; 
-    float p, area;
+    int* ptr = arr;
+    int x, y, z, p, area;
     for (int i=0; i<n; i++){
         x=tr[i].a; y=tr[i].b; z=tr[i].c;
-        p = (double)(x+y+z)/2;
-        area = sqrt(p*(p-x)*(p-y)*(p-z));
-        arr[i] = p;
-    }
-    for(int i=0; i<n; i++){
-        printf("%.1f\n", arr[i]);
+        p = (x+y+z)/2;
+        area = pow(p*(p-x)*(p-y)*(p-z), 0.5);
+        *ptr = area;
     }
 }
 
 int main()
 {
-	int n=2;
+	int n;
+	scanf("%d", &n);
 	triangle *tr = malloc(n * sizeof(triangle));
-    tr[0].a = 3;
-    tr[0].b = 3;
-    tr[0].c = 3;
-    tr[1].a = 4;
-    tr[1].b = 5;
-    tr[1].c = 6;
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
+	}
 	sort_by_area(tr, n);
 	for (int i = 0; i < n; i++) {
 		printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
