b18c04b794279ff830b5d53e48060fef31b59680
Asad <asadahsan2245@gmail.com>
2025-09-08 20:04:13 +0530

    C++ datatypes

---
+++ cpp-language/datatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int x=10;
+    // // long
+    // long x = 55;
+    // cin >> x
+    // long long x;
+    // float x;
+    double x;
+    cout << "Enter num x: ";
+    cin >> x;
+    cout << "Value of x: " << x;
+    return 0;
+}
