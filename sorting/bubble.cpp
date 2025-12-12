f640b3e5ec0a80121590656ee517a4e6f9ba71ef
Asad <asadahsan2245@gmail.com>
2025-12-12 12:57:04 +0530

    bubble done

---
+++ sorting/bubble.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bubble(vector<int>* vec, int n){
+    for(int i=0; i<=n-1; i++){
+        for(int j=0; j<=n-i-2; j++){
+            if((*vec)[j]>(*vec)[j+1]){
+                swap((*vec)[j], (*vec)[j+1]);
+                // for(auto element: *vec) cout << element << " ";
+                // cout << endl;
+            }
+        }
+    }
+};
+
+int main() {
+    vector<int> ls = {10, 12, 9, 4, 1};
+    for(auto element:ls) cout << element << " ";
+    cout << endl;
+    bubble(&ls, 5);
+    for(auto element:ls) cout << element << " ";
+
+    return 0;
+}
