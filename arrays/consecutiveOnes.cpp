2215bc2bfd44489a061906ba83ccb92aafff4d09
Asad <asadahsan2245@gmail.com>
2025-12-23 08:53:07 +0530

    morning revision

--- arrays/consecutiveOnes.cpp
+++ arrays/consecutiveOnes.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+void countOnes(vector<int> &arr){
+    int n=arr.size();
+    int count=0;
+    int max=0;
+    for(int i=0; i<n; i++){
+        if(arr[i]==1) count++;
+        else count=0;  
+        if(count>max) max=count;
+    }
+    cout << max << endl;
+}
 int main() {
-
+    vector<int> ones={1,1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1};
+    countOnes(ones);
     return 0;
 }
--- arrays/findmissingn.cpp
+++ arrays/findmissingn.cpp
@@ -13,9 +13,22 @@ void findMissing(vector<int> arr, int n){
         k++; i++;
     }
 }
+void missing(vector<int> &arr, int n){
+    int xor1=0;
+    int xor2=0;
+    for(int i=0; i<n-1; i++){
+        xor1=xor1^arr[i];
+    }
+    for(int j=1; j<=n; j++){
+        xor2=xor2^(j);
+    }
+    int result = xor1 ^ xor2;
+    cout << result << endl;
+}
 int main() {
-    vector <int> ls = {1, 2, 3, 4, 5, 6, 7, 10};
-    int n=10;
+    vector <int> ls = {1, 2, 3, 4, 5, 6, 7, 9};
+    int n=9;
     findMissing(ls, n);
+    missing(ls, n);
     return 0;
 }
