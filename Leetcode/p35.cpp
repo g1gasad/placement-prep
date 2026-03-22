2b9bd8a8ef08e3effe6a532d29310fcc3a83f69a
Asad <asadahsan2245@gmail.com>
2026-03-22 11:25:16 +0530

    leetcode p35

--- Leetcode/p35.cpp
+++ Leetcode/p35.cpp
@@ -2,17 +2,28 @@
 using namespace std;
 int searchInsert(vector<int>& nums, int target) {
     int n=nums.size();
-    int i=0;
-    while(i<n){
-        if(nums[i]==target) return i;
-        else if(nums[i]>target) return i;
-        i++;
+    int low=0;
+    int high=n-1;
+    int idx=n;
+
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]>target) {
+            idx=mid;
+            high=mid-1;
+        }
+        else low=mid+1;
     }
+    return idx;
 }
 int main() {
     vector<int> arr={1,3,5,10};
-    int target=4;
-    int result=searchInsert(arr, target);
-    printf("the index of target %d is %d", target, result);
+    vector<int> targets = {5, -1, 7, 100, 99, 3, 10, -44, 123, 6};
+    for(int i=0; i<targets.size(); i++){
+        int target=targets[i];
+        int result=searchInsert(arr, target);
+        printf("the index of target %d is %d\n", target, result);
+    }
     return 0;
 }
