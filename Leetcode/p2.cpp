45efb98252b16a3aa065d8920d0d2940180f6ee1
Asad <asadahsan2245@gmail.com>
2026-06-06 15:08:41 +0530

    solved LL q2

--- Leetcode/p2.cpp
+++ Leetcode/p2.cpp
@@ -48,51 +48,55 @@ void printElement(ListNode* head){
 }
 class Solution {
 private:
-    long long powerof10(int x){
-        if(x==0) return 1;
-        if(x==1) return 10;
-        long long n=10;
-        for(int i=1; i<x; i++){
-            n = n*10;
-        }
-        return n;
+    int ifCarry(int value){
+        if(value>9) return 1;
+        else return 0;
     }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        long long num1=0, num2=0;
-        int i=0; int j=0;
-        while(l1 != NULL){
-            int d=l1->val;
-            // cout << d;
-            num1 = powerof10(i)*d + num1;
-            l1=l1->next;
-            i++;
-        }
-        while(l2 != NULL){
-            int d=l2->val;
-            // cout << d;
-            num2 = powerof10(j)*d + num2;
+        int sum1 = (l1->val + l2->val);
+        int carry = ifCarry(sum1);
+
+        ListNode* head = new ListNode(sum1%10);
+        l1=l1->next; l2=l2->next;
+        ListNode* mover=head;
+        
+        while(l1!=NULL && l2!=NULL){
+            int d=l1->val + l2->val + carry;
+            // cout << d << endl;
+            ListNode* temp= new ListNode(d%10);
+            mover->next=temp;
+            carry=ifCarry(d);
+            l1=l1->next; 
             l2=l2->next;
-            j++;
+            mover=mover->next;
         }
-        long long sum = num1+num2;
-        int dig=sum%10;
-        ListNode* head=new ListNode(dig);
-        sum=sum/10;
-        ListNode* temp=head;
-        while(sum){
-            dig = sum%10;
-            ListNode* newNode = new ListNode(dig);
-            temp->next=newNode;
-            temp=temp->next;
-            sum /= 10;
+        while(l2!=NULL){
+            int d = l2->val + carry;
+            ListNode* temp= new ListNode(d%10);
+            mover->next=temp;
+            carry=ifCarry(d);
+            l2=l2->next;
+            mover=mover->next;
+        }
+        while(l1!=NULL){
+            int d = l1->val + carry;
+            ListNode* temp= new ListNode(d%10);
+            mover->next=temp;
+            carry=ifCarry(d);
+            l1=l1->next;
+            mover=mover->next;
+        }
+        if(carry){
+            ListNode* temp = new ListNode(carry);
+            mover->next=temp;
         }
         return head;
     }
 };
 int main(){
-    vector<int> arr1 = {2, 4, 3, 9, 9, 9, 9, 9, 9, 9, 9};
-    vector<int> arr2 = {5, 6, 4};
+    vector<int> arr1 = {2, 4};
+    vector<int> arr2 = {5, 6, 9, 9, 9, 9, 9, 9};
     ListNode* head1 = convertArr2LL(arr1);
     ListNode* head2 = convertArr2LL(arr2);
 
--- Leetcode/tempCodeRunnerFile.cpp
+++ Leetcode/tempCodeRunnerFile.cpp
@@ -1,6 +1,4 @@
-class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-//     }
-// };
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        long long num1=0, num2=0;
+        int i=0; int j=0;
